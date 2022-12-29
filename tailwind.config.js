ad3cfd10b506a27d507aea96470f5f62930cd457
Dhananjayan D <dhananjayan.dayalan@gmail.com>
2022-12-29 15:41:20 +0530

    changes in tailwind config and script js type to module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['*.{html, js}'],
-  content: ['*.{html, js}'],
+  content: ['./*.{html,js}'],
   theme: {
-    extend: { 
-    }
+    extend: {}
   },
   plugins: []
 };
